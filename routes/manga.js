cbd265bbafc7bcc1e91a75e3ec1afc0d129031d5
MichaelPNair <michael.p.nair@gmail.com>
2023-09-21 23:17:36 +1000

    change manga search route to put instead of get

--- routes/manga.js
+++ routes/manga.js
@@ -3,15 +3,18 @@ const router = express.Router()
 const AppError = require('../lib/app_error')
 const axios = require('axios')
 
-router.get('/:id', async (req, res, next) => {
-    axios.get(`https://api.mangadex.org/manga/${req.params.id}?includes%5B%5D=manga&includes%5B%5D=cover_art&includes%5B%5D=author&includes%5B%5D=artist`)
-        .then(manga => res.json(manga.data))
-        .catch(next)
 
+router.put('/search', async (req, res, next) => {
+    const {searchText} = req.body
+
+    axios.get(`https://api.mangadex.org/manga?limit=9&title=${searchText}&includedTagsMode=AND&excludedTagsMode=OR&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&order%5Brelevance%5D=desc&includes%5B%5D=manga&includes%5B%5D=cover_art&includes%5B%5D=author&includes%5B%5D=artist`)
+    .then(manga => res.json(manga.data))
+    .catch(next)
+    
 })
 
-router.get('/search/:searchText', async (req, res, next) => {
-    axios.get(`https://api.mangadex.org/manga?limit=9&title=${req.params.searchText}&includedTagsMode=AND&excludedTagsMode=OR&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&order%5Brelevance%5D=desc&includes%5B%5D=manga&includes%5B%5D=cover_art&includes%5B%5D=author&includes%5B%5D=artist`)
+router.get('/:id', async (req, res, next) => {
+    axios.get(`https://api.mangadex.org/manga/${req.params.id}?includes%5B%5D=manga&includes%5B%5D=cover_art&includes%5B%5D=author&includes%5B%5D=artist`)
         .then(manga => res.json(manga.data))
         .catch(next)
 

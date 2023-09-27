8b2be050f095ec7f471a0399a81355666f8f90f4
MichaelPNair <michael.p.nair@gmail.com>
2023-09-27 21:27:00 +1000

    fix manga seach to exclude doujinshi

--- routes/manga.js
+++ routes/manga.js
@@ -7,7 +7,7 @@ const axios = require('axios')
 router.put('/search', async (req, res, next) => {
     const {searchText} = req.body
 
-    axios.get(`https://api.mangadex.org/manga?limit=9&title=${searchText}&includedTagsMode=AND&excludedTagsMode=OR&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&order%5Brelevance%5D=desc&includes%5B%5D=manga&includes%5B%5D=cover_art&includes%5B%5D=author&includes%5B%5D=artist`)
+    axios.get(`https://api.mangadex.org/manga?limit=20&title=${searchText}&includedTagsMode=AND&excludedTagsMode=OR&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&order%5Brelevance%5D=desc&includes%5B%5D=manga&includes%5B%5D=cover_art&includes%5B%5D=author&includes%5B%5D=artist`)
     .then(manga => res.json(manga.data))
     .catch(next)
     
